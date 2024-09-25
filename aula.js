e8765ca7458a247c0735944c160b75e96229d008
BiancagscCabral <biancagsccabral@gmail.com>
2024-09-25 10:39:53 -0300

    criei o repositório

---
+++ aula.js
@@ -0,0 +1,9 @@
+console.log('hello'+ '' +'World')
+
+const myName = 'bianca'
+console.log(`Hello, ${myName}`)
+
+myName2 ="2"
+console.log(`Hello ${myName2}`)
+
+console.log(2 == 'casa')
---
+++ aula2.js
@@ -0,0 +1,15 @@
+console.log('hello'+' '+'wolrd')
+console.log('hello'+'world')
+ 
+myName = "Maria"
+console.log("Hello" +''+myName)
+myName2="2"
+console.log(`hello ${myName2}`)
+var soma = 10 + "25"
+console.log(soma)
+ 
+console.log(2==2)
+console.log(2== "casa")
+console.log(2==="2")
+console.log(2== "casa")
+ 
