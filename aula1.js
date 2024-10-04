8dca73ea758bee622ad3442d935d500d2851445e
Bianca Guimarães <164365196+BiancagscCabral@users.noreply.github.com>
2024-10-04 10:45:08 -0300

    aula sexta arnott

---
+++ aula1.js
@@ -0,0 +1,20 @@
+/*ls (caso a gente execute o comando node e não tenha entrado na pasta)
+cd (nome da pasta)
+ls
+node aula.js(pode ser qualquer nome) */
+
+
+
+// aula 04/10 do professor Arnott
+
+
+
+let valorfone = 120
+let valorrifa = 3.00
+let numero = 100
+console.log(numero * valorrifa - valorfone)
+
+//180
+
+
+
